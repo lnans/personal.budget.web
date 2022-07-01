a0c711f14878ff3813263f7cb053c383cb69e461
Nans Laupretre <nans.laupretre@gmail.com>
2022-07-01 21:22:35 +0200

    chore - add comments on utils functions

--- src/utils/enum.ts
+++ src/utils/enum.ts
@@ -1,8 +1,13 @@
 import { i18n } from 'i18next'
 
-// Turn enum into array
+/**
+ * Turn enum into array
+ * @param enumme Enum to transform
+ * @param name Name of the enum
+ * @param i18n I18n instance for translate label
+ * @returns Enum list object
+ */
 export function EnumToSelect<TEnum extends { [s: string]: string }>(enumme: TEnum, name: string, i18n: i18n) {
-  //const list = []
   return Object.keys(enumme).map((value) => ({
     id: value,
     value: i18n.t(`enums.${name}.${value}`),
--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,5 +1,11 @@
-export function numberWithSpaces(x: number) {
-  var parts = x.toString().split('.')
+/**
+ * Transorm number to string spaced number
+ * ex: 12452 -> 12 452
+ * @param value Number to transform
+ * @returns Transformed number as string
+ */
+export function numberWithSpaces(value: number) {
+  var parts = value.toString().split('.')
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
   return parts.join('.')
 }
