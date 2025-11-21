8fde92871c15b86fb8acf7c04bb2ec885636d4e0
Nans Laupretre <nans.laupretre@gmail.com>
2025-11-21 20:38:54 +0100

    chore(rebirth): recreate repo with new librairies

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from '@eslint/js'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import prettier from 'eslint-plugin-prettier/recommended'
+import reactDom from 'eslint-plugin-react-dom'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import reactX from 'eslint-plugin-react-x'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig([
+  globalIgnores(['dist', '.yarn']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      prettier,
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
+      ...pluginQuery.configs['flat/recommended'],
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+])
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/Button'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+    </>
+  )
+}
+
+export default App
