dcbcdd13b95470c440f2f29863b39649c221a00c
Nans Laupretre <nans.laupretre@gmail.com>
2025-12-26 14:58:08 +0100

    feat(ui): implement MainPage with sidebar navigation and layout structure

--- src/app/main/MainPage.tsx
+++ src/app/main/MainPage.tsx
@@ -1,5 +1,125 @@
-function MainPage() {
-  return <div>MainPage</div>
+import { GalleryVerticalEnd, LayoutDashboard, Settings } from 'lucide-react'
+import * as React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { Card } from '@/components/ui/Card'
+import { Separator } from '@/components/ui/Separator'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/Sidebar'
+
+// TODO: put this in a hook or config file
+const data = {
+  navMain: [
+    {
+      title: 'Dashboard',
+      url: '#',
+      icon: <LayoutDashboard />,
+    },
+  ],
+  navSecondary: [
+    {
+      title: 'Settings',
+      url: '#',
+      icon: <Settings />,
+    },
+  ],
 }
 
-export { MainPage }
+export function MainPage() {
+  const { t } = useTranslation()
+  return (
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)',
+        } as React.CSSProperties
+      }
+    >
+      <Sidebar collapsible="offcanvas" variant="inset">
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild className="data-[slot=sidebar-menu-button]:p-1.5!">
+                <a href="#">
+                  <GalleryVerticalEnd className="size-5!" />
+                  <span className="text-base font-semibold">{t('app.brand')}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
+
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent className="flex flex-col gap-2">
+              <SidebarMenu>
+                {data.navMain.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton tooltip={item.title}>
+                      {item.icon && <React.Fragment>{item.icon}</React.Fragment>}
+                      <span>{t(item.title)}</span>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+
+          <SidebarGroup className="mt-auto">
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {data.navSecondary.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <React.Fragment>{item.icon}</React.Fragment>
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+
+      <SidebarInset>
+        <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+          <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
+            {/* TODO: maybe use a breadcrumb here? or maybe i18n to transaltion the location pathname? */}
+            <h1 className="text-base font-medium">Documents</h1>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              {/* TODO: use Outlet */}
+              <div>
+                {Array.from({ length: 20 }).map((_, index) => (
+                  <Card className="m-2 p-4" key={index}>
+                    Data test {index}
+                  </Card>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
