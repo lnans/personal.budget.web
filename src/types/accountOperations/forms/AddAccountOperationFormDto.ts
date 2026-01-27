cca6f7f1bdb07a71183d5d9b0ac4727ff840ee2a
Nans Laupretre <nans.laupretre@gmail.com>
2026-01-27 19:49:56 +0100

    chore(accounts): rename add account types + update packages

--- src/types/accountOperations/forms/AddAccountOperationFormDto.ts
+++ src/types/accountOperations/forms/AddAccountOperationFormDto.ts
@@ -2,9 +2,9 @@ import { z } from 'zod'
 
 const MAX_DESCRIPTION_LENGTH = 100
 
-export const AddOperationFormSchema = z.object({
+export const AddAccountOperationFormSchema = z.object({
   description: z.string().min(1).max(MAX_DESCRIPTION_LENGTH),
   amount: z.number(),
 })
 
-export type AddOperationFormDto = z.infer<typeof AddOperationFormSchema>
+export type AddAccountOperationFormDto = z.infer<typeof AddAccountOperationFormSchema>
---
+++ src/types/accountOperations/responses/AddAccountOperationResponseDto.ts
@@ -0,0 +1,10 @@
+export type AddAccountOperationResponseDto = {
+  id: string
+  accountId: string
+  description: string
+  amount: number
+  previousBalance: number
+  nextBalance: number
+  createdAt: string
+  updatedAt: string
+}
